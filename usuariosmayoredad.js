2efe904c55c324535c832e11d323eef61494841e
tebyortiz <tebyortiz@gmail.com>
2023-04-06 14:49:43 -0300

    required modification

--- usuariosmayoredad.js
+++ usuariosmayoredad.js
@@ -1,5 +1,12 @@
 let edadesUsuarios = [18, 22, 30, 15, 45, 17, 55, 9, 7, 12];
 
-function recorrerListado(lista) {
 
+function recorrerListado(lista) {
+    for(let i = 0; i < lista.length; i++) {
+        if(lista[i] >= 18) {
+            console.log(lista[i])
+        }
+    }
 }
+let lista = edadesUsuarios
+recorrerListado(lista)
